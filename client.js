6a295bc0e19acb2d281e34443ae76207e446faeb
Reoberg <onuroziskender@outlook.com>
2024-01-31 15:44:24 +0000

    Instance.

---
+++ client.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('get-nominations').addEventListener('click', getNominations);
+    document.getElementById('get-nominees').addEventListener('click', getNominees);
+    document.getElementById('clear-input').addEventListener('click', clearInput);
+    document.getElementById('clear-output').addEventListener('click', clearOutput);
+});
+
+function getNominations() {
+}
+
+function getNominees() {
+}
+
+function clearInput() {
+}
+
+function clearOutput() {
+}
